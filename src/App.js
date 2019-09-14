934d0ed3d702d8ede1ade558e016c8e6684d40b4
Alan Yoset García Cruz <alancrux_@hotmail.com>
2019-09-14 11:42:14 -0500

    cambios dos

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Segunda edición papu :v <code>src/App.js</code> and save to reload.
+          Oie <code>Picale aquí</code> y no pasa nada.
         </p>
         <a
           className="App-link"
